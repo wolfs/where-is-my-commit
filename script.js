24c21c646b63b2dbc31e7eb7c23829e2d4bf0d6a
Stefan Wolf <wolfs@fs.tum.de>
2015-03-06 23:55:05 +0100

    Polling works

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 var whereIsMyBuild = function() {
+	var getQueryVariable = function(variable) {
+		var query = window.location.search.substring(1);
+		var vars = query.split("&");
+		for (var i = 0; i < vars.length; i++) {
+			var pair = vars[i].split("=");
+			if (pair[0] == variable) {
+				return pair[1];
+			}
+		}
+		return (false);
+	}
+
 	var baseBuildNode = {
 		getName: function() {
 			return this.jobName + " - " + this.revision
@@ -12,17 +24,19 @@ var whereIsMyBuild = function() {
 
 		return n;
 	}
-	var data = buildNode("chain-start", "1234")
+	var data = buildNode("chain-start", parseInt(getQueryVariable("revision"), 10))
 	var toUpdate = [data]
 	var when = function(deferreds) {
 		if (deferreds.length == 0) {
 			return $.Deferred().resolve([]);
 		} else if (deferreds.length == 1) {
-			return deferreds[0].pipe(function(deferred) {
+			return deferreds[0].then(function(deferred) {
 				return [deferred]
 			})
 		} else {
-			return $.when.apply($, deferreds)
+			return $.when.apply($, deferreds).then(function() {
+				return $.makeArray(arguments);
+			})
 		}
 	}
 
@@ -39,8 +53,6 @@ var whereIsMyBuild = function() {
 		.attr("height", height)
 		.append("g")
 		.attr("transform", "translate(500,300)");
-
-	var searchedRev = 1234;
 
 	var renderData = function() {
 		var nodes = cluster.nodes(data),
@@ -63,10 +75,17 @@ var whereIsMyBuild = function() {
 				return d.getName();
 			});
 
+		var arc = d3.svg.arc()
+			.innerRadius(10)
+			.outerRadius(20)
+			.startAngle(0)
+			.endAngle(6.28)
+
 		var parentNode = node.enter().append("g")
 			.attr("class", "node");
-		parentNode.append("circle")
-			.attr("r", 4.5);
+		parentNode.append("path")
+			.attr("class", "pending")
+			.attr("d", arc);
 		var textNode = parentNode.append("a")
 			.attr("xlink:href", function(d) {
 				return d.url
@@ -74,7 +93,7 @@ var whereIsMyBuild = function() {
 			.append("text");
 
 		var dxChildren = function(d) {
-			return 8;
+			return 40;
 		}
 
 		textNode
@@ -85,6 +104,7 @@ var whereIsMyBuild = function() {
 
 		textNode
 			.append("tspan")
+			.attr("class", "revision")
 			.attr("dy", "1.2em")
 			.text(function(d) {
 				return d.revision
@@ -94,10 +114,19 @@ var whereIsMyBuild = function() {
 			.attr("x", "0")
 			.attr("dx", dxChildren)
 
+		node.selectAll("path")
+			.attr("class", function(d) {
+				return d.status;
+			})
 
 		node.selectAll("a text").transition()
 			.attr("dy", 0)
 			.style("text-anchor", "start");
+
+		node.selectAll("a text tspan.revision").transition()
+			.text(function(d) {
+				return d.revision;
+			})
 
 		node.transition().attr("transform", function(d) {
 			return "translate(" + d.x + "," + d.y + ")";
@@ -113,17 +142,63 @@ var whereIsMyBuild = function() {
 			for (var i = 0; i < envVars.length; i++) {
 				var env = envVars[i];
 				if (env.name == "REV") {
-					return env.value
+					return parseInt(env.value, 10);
 				}
 			}
 		}
+
+		var buildKeys = "number,url,result,actions[triggeredProjects[name]]";
+
 		var jobRequest = $.getJSON(
 			"http://localhost:8080/job/" + jobName +
-			"/api/json?tree=lastBuild[number,url,actions[triggeredProjects[name]]]")
-		var envVarsRequest = jobRequest.pipe(
-			function(job) {
-				return $.getJSON(job.lastBuild.url + "injectedEnvVars/export");
+			"/api/json?tree=lastCompletedBuild[" + buildKeys + "]"
+		);
+
+		var buildDef = jobRequest.then(function(job) {
+			return job.lastCompletedBuild;
+		});
+
+		var getEnvVars = function(build) {
+			return build == undefined ? undefined : $.getJSON(build.url +
+				"injectedEnvVars/export");
+		}
+		var getRevision = function(build) {
+			if (build == undefined) {
+				return undefined;
+			}
+			return getEnvVars(build).then(function(envVars) {
+				return findRevision(envVars.envVars.envVar)
 			})
+		}
+		var envVarsRequest = buildDef.then(getEnvVars);
+
+		var buildForRevision = function(buildDef) {
+			return $.when(buildDef, buildDef.then(getRevision)).then(
+				function(build, revision) {
+					if (build == undefined) {
+						return undefined;
+					}
+					build.revision = revision;
+					if (revision < nodeToUpdate.revision) {
+						return undefined;
+					} else if (revision == nodeToUpdate.revision) {
+						return build;
+					} else {
+						return buildForRevision($.getJSON(
+								"http://localhost:8080/job/" + jobName + "/" + (build.number - 1) +
+								"/api/json?tree=" + buildKeys)
+							.then(function(build) {
+								return build;
+							})).then(function(previousBuild) {
+							if (previousBuild == undefined) {
+								return build;
+							} else {
+								return previousBuild;
+							}
+						})
+					}
+				})
+		}
 
 		var getTriggeredProjects = function(build) {
 			var actions = build.actions;
@@ -139,42 +214,49 @@ var whereIsMyBuild = function() {
 			return triggeredProjects;
 		}
 
-		var triggeredProjectRequest = jobRequest.pipe(
-			function(job) {
-				var triggeredProjects = getTriggeredProjects(job.lastBuild)
+		buildForRevision(buildDef).then(function(build) {
+			if (build == undefined) {
+				nodeToUpdate.status = "pending";
+				toUpdate.push(nodeToUpdate);
+				resultDef.resolve(nodeToUpdate);
+			} else {
+				nodeToUpdate.status = build.result.toLowerCase();
+				nodeToUpdate.revision = build.revision;
+				nodeToUpdate.url = build.url;
+
+				var triggeredProjects = getTriggeredProjects(build);
 				var deferreds = $.map(triggeredProjects, function(job) {
 					return buildData(buildNode(job.name, nodeToUpdate.revision));
 				});
 
-				return when(deferreds);
+				when(deferreds).then(function(triggeredBuilds) {
+					nodeToUpdate.children = triggeredBuilds;
+					resultDef.resolve(nodeToUpdate);
+				})
 			}
-		)
-
-		$.when(jobRequest, envVarsRequest, triggeredProjectRequest).done(
-			function(jobResult, envVarsResult, triggeredProjects) {
-				var job = jobResult[0];
-				var envVars = envVarsResult[0];
-				var rev = findRevision(envVars.envVars.envVar)
-					// if (!$.isArray(triggeredProjects)) {
-					// 	triggeredProjects = [triggeredProjects]
-					// }
-
-				nodeToUpdate.revision = rev;
-				nodeToUpdate.url = job.lastBuild.url;
-				nodeToUpdate.children = triggeredProjects;
-				resultDef.resolve(nodeToUpdate)
-			}
-		)
+		}, function() {
+			toUpdate.push(nodeToUpdate);
+		})
 
 		return resultDef;
+	}
+
+	var updateNext = function() {
+		if (toUpdate.length > 0) {
+			var current = toUpdate.shift();
+			buildData(current);
+		}
 	}
 
 	var init = function() {
 		d3.select(self.frameElement).style("height", height + "px");
 
-		buildData(toUpdate[0]).done(function(result) {
-			renderData();
-		})
+		updateNext();
+		renderData();
+
+		setInterval(updateNext, 2000)
+
+		setInterval(renderData, 2000)
 	}
 
 	return {
