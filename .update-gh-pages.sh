972f5c7baf88ba59bff5ce99d561043ab089cb0b
Andreas Greimel <andreas@greimel.de>
2015-04-09 13:15:07 +0200

    Shell script for deployment

---
+++ .update-gh-pages.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to update gh-pages\n"
+
+  #copy data we're interested in to other place
+  cp -R dist $HOME/dist
+
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+
+  #using token clone gh-pages branch
+  git clone --quiet --branch=gh-pages https://${TOKEN}@github.com/greimela/where-is-my-commit.git  gh-pages > /dev/null
+
+  #go into diractory and copy data we're interested in to that directory
+  cd gh-pages
+  cp -Rf $HOME/dist/ .
+
+  #add, commit and push files
+  git add -f .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Done magic with coverage\n"
+fi
