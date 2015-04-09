874c4de47c4659634a5c069645d10057f0fda852
Stefan Wolf <wolfs@fs.tum.de>
2015-04-09 22:13:51 +0200

    Removed comment

--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
 require.config({
   baseUrl: '',
   paths: {
-    // the left side is the module ID,
-    // the right side is the path to
-    // the jQuery file, relative to baseUrl.
-    // Also, the path should NOT include
-    // the '.js' file extension. This example
-    // is using jQuery 1.9.0 located at
-    // js/lib/jquery-1.9.0.js, relative to
-    // the HTML page.
     jquery: '../bower_components/jquery/dist/jquery.min',
     d3: '../bower_components/d3/d3.min'
   },
