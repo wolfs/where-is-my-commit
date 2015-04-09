237a48045dd6c41a156c188d05cc351af5cee2a3
Andreas Greimel <andreas@greimel.de>
2015-04-09 13:46:50 +0200

    Update update-gh-pages.sh

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -10,7 +10,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.name "Travis"
 
   #using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://${TOKEN}@github.com/greimela/where-is-my-commit.git  gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://$TOKEN@github.com/greimela/where-is-my-commit.git  gh-pages > /dev/null
 
   #go into diractory and copy data we're interested in to that directory
   cd gh-pages
