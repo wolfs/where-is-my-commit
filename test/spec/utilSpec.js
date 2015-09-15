c23d24cfc147d9c71f090dcc6c23f53aedeb257f
Stefan Wolf <wolfs@fs.tum.de>
2015-09-15 23:26:12 +0200

    Some tests for query String parsing

--- test/spec/utilSpec.js
+++ test/spec/utilSpec.js
@@ -1,12 +1,34 @@
-define(['common/util'], function(util){
+define(['common/util'], function (util) {
   'use strict';
-  describe("getQueryVariable", function(){
-    it("Should extract the Query Variable", function(){
+  describe("getQueryVariable", function () {
+    it("Should extract the Query Variable", function () {
       expect(util.getQueryVariableFromSearch('revision', '?revision=1234')).toEqual("1234");
     });
 
-    it("Should extract the Query Variable from multiple queries", function(){
+    it("Should extract the Query Variable from multiple queries", function () {
       expect(util.getQueryVariableFromSearch('revision', '?stuff=14&revision=1234&somethingElse=blah')).toEqual("1234");
+    });
+
+    it("Should extract an Object for the query", function () {
+      expect(util.queryVariablesFromQuery('stuff=14&revision=1234&somethingElse=blah')).toEqual(
+        {
+          stuff: '14',
+          revision: '1234',
+          somethingElse: 'blah'
+        });
+    });
+
+    it("Should parse + correctly", function () {
+      expect(util.queryVariablesFromQuery('stuff=14&revision=1234&somethingElse=blah+Important')).toEqual(
+        {
+          stuff: '14',
+          revision: '1234',
+          somethingElse: 'blah Important'
+        });
+    });
+
+    it("Should parse an empty query", function () {
+      expect(util.queryVariablesFromQuery('')).toEqual({});
     });
   });
 });
