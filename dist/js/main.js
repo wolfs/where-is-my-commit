f799cfd42193d8cc38e41dabf5adf57b42fcde1f
Travis <travis@travis-ci.org>
2015-09-15 06:14:18 +0000

    Travis build 54 pushed to gh-pages

--- dist/js/main.js
+++ dist/js/main.js
@@ -932,124 +932,6 @@ define('broken/renderer', [
     };
     return my;
 });
-define('broken/controller', [
-    'jquery',
-    'common/util',
-    'app-config',
-    'broken/builds',
-    'broken/updater',
-    'broken/renderer'
-], function ($, util, config, data, updater, renderer) {
-    var my = {}, throttler = util.newThrottler(config.bulkUpdateSize, config.coreUpdateInterval);
-    var initFormSubmit = function () {
-        var selectedTestCases = function () {
-            var selected = $('input.testCaseSelect:checked');
-            var ids = $.makeArray(selected.map(function () {
-                return $(this).data('testcaseid');
-            }));
-            return ids.map(function (id) {
-                return data.testCaseForId(id);
-            });
-        };
-        var selectedBuilds = function () {
-            var selected = $('input.buildSelect:checked');
-            var ids = $.makeArray(selected.map(function () {
-                return $(this).data('buildid');
-            }));
-            return ids.map(function (id) {
-                return data.buildForId(id);
-            });
-        };
-        $('#claimForm').submit(function (event) {
-            try {
-                var testCases = selectedTestCases();
-                var builds = selectedBuilds();
-                var claim = {};
-                $(this).serializeArray().forEach(function (field) {
-                    claim[field.name] = field.value;
-                });
-                util.sequentially(testCases.concat(builds), function (testCase) {
-                    return updater.claim(testCase, claim);
-                });
-            } catch (err) {
-                console.log(err);
-            }
-            event.preventDefault();
-        });
-        $('#dropClaimsForm').submit(function (event) {
-            try {
-                var testCases = selectedTestCases();
-                var builds = selectedBuilds();
-                testCases.concat(builds).forEach(updater.unclaim);
-            } catch (err) {
-                console.log(err);
-            }
-            event.preventDefault();
-        });
-    };
-    var progressUpdater = function (number) {
-        var my = {};
-        my.total = number;
-        my.current = 0;
-        my.callback = function () {
-            my.current++;
-            my.updateProgress();
-        };
-        my.updateProgress = function () {
-            $('#loadingProgress').width(my.current * 100 / my.total + '%');
-        };
-        return my;
-    };
-    my.init = function (urlsDef) {
-        initFormSubmit();
-        updater.views().then(renderer.addViews);
-        renderer.renderLoop();
-        urlsDef.then(function (urls) {
-            var progress = progressUpdater(urls.length * 2);
-            throttler.scheduleUpdates(urls.map(function (url) {
-                return function () {
-                    updater.addForUrl(url, progress.callback);
-                };
-            }));
-        }, function (error, statusCode, statusText) {
-            var loading = $('#projects').find('.loading')[0];
-            loading.innerHTML = '<div class="alert alert-danger" role="alert">Loading Failed: ' + statusText + '</div>';
-        });
-        urlsDef.then(function (urls) {
-            if (urls.length === 0) {
-                var loading = $('#projects').find('.loading')[0];
-                loading.innerHTML = '<div class="alert alert-warning" role="alert">No projects found - please select a view</div>';
-            }
-        });
-    };
-    return my;
-});
-define('broken/lastBuildsOf', [
-    'jquery',
-    'app-config'
-], function ($, config) {
-    var my = {};
-    var defaultSelector = 'lastCompletedBuild';
-    my.multijob = function (multijobName, selectorArg) {
-        var selector = selectorArg || defaultSelector, multijobUrl = config.jenkinsUrl + '/job/' + multijobName + '/' + selector + '/api/json?tree=subBuilds[url]';
-        return $.getJSON(multijobUrl).then(function (multijobBuild) {
-            return multijobBuild.subBuilds.map(function (subBuild) {
-                return config.jenkinsUrl + '/' + subBuild.url;
-            });
-        });
-    };
-    my.view = function (viewName, selectorArg) {
-        var selector = selectorArg || defaultSelector, viewUrl = config.jenkinsUrl + '/view/' + viewName + '/api/json?tree=jobs[url,color]';
-        return $.getJSON(viewUrl).then(function (view) {
-            return view.jobs.filter(function (job) {
-                return job.color !== 'blue';
-            }).map(function (job) {
-                return job.url + selector + '/';
-            });
-        });
-    };
-    return my;
-});
 !function (a, b) {
     'object' == typeof module && module.exports ? module.exports = b() : 'function' == typeof define && define.amd ? define('spin', [], b) : a.Spinner = b();
 }(this, function () {
@@ -1234,6 +1116,150 @@ define('broken/lastBuildsOf', [
         !d(o, 'transform') && o.adj ? i() : j = d(o, 'animation');
     }
     return h;
+});
+define('broken/controller', [
+    'jquery',
+    'common/util',
+    'app-config',
+    'broken/builds',
+    'broken/updater',
+    'broken/renderer',
+    'spin'
+], function ($, util, config, data, updater, renderer, Spinner) {
+    var my = {}, throttler = util.newThrottler(config.bulkUpdateSize, config.coreUpdateInterval);
+    var initFormSubmit = function () {
+        var selectedTestCases = function () {
+            var selected = $('input.testCaseSelect:checked');
+            var ids = $.makeArray(selected.map(function () {
+                return $(this).data('testcaseid');
+            }));
+            return ids.map(function (id) {
+                return data.testCaseForId(id);
+            });
+        };
+        var selectedBuilds = function () {
+            var selected = $('input.buildSelect:checked');
+            var ids = $.makeArray(selected.map(function () {
+                return $(this).data('buildid');
+            }));
+            return ids.map(function (id) {
+                return data.buildForId(id);
+            });
+        };
+        $('#claimForm').submit(function (event) {
+            try {
+                var testCases = selectedTestCases();
+                var builds = selectedBuilds();
+                var claim = {};
+                $(this).serializeArray().forEach(function (field) {
+                    claim[field.name] = field.value;
+                });
+                util.sequentially(testCases.concat(builds), function (testCase) {
+                    return updater.claim(testCase, claim);
+                });
+            } catch (err) {
+                console.log(err);
+            }
+            event.preventDefault();
+        });
+        $('#dropClaimsForm').submit(function (event) {
+            try {
+                var testCases = selectedTestCases();
+                var builds = selectedBuilds();
+                testCases.concat(builds).forEach(updater.unclaim);
+            } catch (err) {
+                console.log(err);
+            }
+            event.preventDefault();
+        });
+    };
+    var progressUpdater = function (number) {
+        var my = {};
+        my.total = number;
+        my.current = 0;
+        my.callback = function () {
+            my.current++;
+            my.updateProgress();
+        };
+        my.updateProgress = function () {
+            $('#loadingProgress').width(my.current * 100 / my.total + '%');
+            if (my.current === my.total) {
+                $('#loadingSpinner').html('<span class="label label-success"><span class="glyphicon glyphicon-ok"></span></span>');
+            }
+        };
+        new Spinner({
+            lines: 13,
+            length: 28,
+            width: 14,
+            radius: 42,
+            scale: 0.1,
+            corners: 1,
+            color: '#000',
+            opacity: 0.25,
+            rotate: 0,
+            direction: 1,
+            speed: 1,
+            trail: 60,
+            fps: 20,
+            zIndex: 2000000000,
+            className: 'spinner',
+            top: '50%',
+            left: '100%',
+            shadow: false,
+            hwaccel: false,
+            position: 'absolute'
+        }).spin($('#loadingSpinner')[0]);
+        return my;
+    };
+    my.init = function (urlsDef) {
+        initFormSubmit();
+        updater.views().then(renderer.addViews);
+        renderer.renderLoop();
+        urlsDef.then(function (urls) {
+            var progress = progressUpdater(urls.length * 2);
+            throttler.scheduleUpdates(urls.map(function (url) {
+                return function () {
+                    updater.addForUrl(url, progress.callback);
+                };
+            }));
+        }, function (error, statusCode, statusText) {
+            var loading = $('#projects').find('.loading')[0];
+            loading.innerHTML = '<div class="alert alert-danger" role="alert">Loading Failed: ' + statusText + '</div>';
+        });
+        urlsDef.then(function (urls) {
+            if (urls.length === 0) {
+                var loading = $('#projects').find('.loading')[0];
+                loading.innerHTML = '<div class="alert alert-warning" role="alert">No projects found - please select a view</div>';
+            }
+        });
+    };
+    return my;
+});
+define('broken/lastBuildsOf', [
+    'jquery',
+    'app-config'
+], function ($, config) {
+    var my = {};
+    var defaultSelector = 'lastCompletedBuild';
+    my.multijob = function (multijobName, selectorArg) {
+        var selector = selectorArg || defaultSelector, multijobUrl = config.jenkinsUrl + '/job/' + multijobName + '/' + selector + '/api/json?tree=subBuilds[url]';
+        return $.getJSON(multijobUrl).then(function (multijobBuild) {
+            return multijobBuild.subBuilds.map(function (subBuild) {
+                return config.jenkinsUrl + '/' + subBuild.url;
+            });
+        });
+    };
+    my.view = function (viewName, selectorArg) {
+        var selector = selectorArg || defaultSelector, viewUrl = config.jenkinsUrl + '/view/' + viewName + '/api/json?tree=jobs[url,color]';
+        return $.getJSON(viewUrl).then(function (view) {
+            return view.jobs.filter(function (job) {
+                return job.color !== 'blue';
+            }).map(function (job) {
+                return job.url + selector + '/';
+            });
+        });
+    };
+    return my;
 });
 define('broken/init', [
     'jquery',
