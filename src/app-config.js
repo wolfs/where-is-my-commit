93ce49c2e1ec9f44dab97334c3ca83fc2a288d23
Stefan Wolf <wolfs@fs.tum.de>
2016-01-02 14:30:21 -0300

    Load my-config by adding it as dependency to the script. Finally, configuration is done by a global variable.

--- src/app-config.js
+++ src/app-config.js
@@ -1,15 +1,20 @@
 define(['jquery'], function ($) {
   'use strict';
-  var globalConfig = {};
-  return {
-    width: globalConfig.width || ($("#graph").width()),
-    height: globalConfig.height || 2000,
-    jenkinsUrl: globalConfig.jenkinsUrl || "http://localhost:8080",
-    startJob: globalConfig.startJob || "chain-start",
-    updateInterval: globalConfig.updateInterval || 2000,
-    coreUpdateInterval: globalConfig.coreUpdateInterval || globalConfig.updateInterval || 2000,
-    commitUpdateInterval: globalConfig.commitUpdateInterval || 20000,
-    bulkUpdateSize: globalConfig.bulkUpdateSize || 10,
-    filterWarnings: globalConfig.filterWarnings || []
+  var globalConfig = (window && window.whereIsMyCommit) || {};
+
+  var mergeWithDefault = function (globalConfig) {
+    return {
+      width: globalConfig.width || ($("#graph").width()),
+      height: globalConfig.height || 2000,
+      jenkinsUrl: globalConfig.jenkinsUrl || "http://localhost:8080",
+      startJob: globalConfig.startJob || "chain-start",
+      updateInterval: globalConfig.updateInterval || 2000,
+      coreUpdateInterval: globalConfig.coreUpdateInterval || globalConfig.updateInterval || 2000,
+      commitUpdateInterval: globalConfig.commitUpdateInterval || 20000,
+      bulkUpdateSize: globalConfig.bulkUpdateSize || 10,
+      filterWarnings: globalConfig.filterWarnings || []
+    };
   };
+
+  return mergeWithDefault(globalConfig);
 });
